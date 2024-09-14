9898061a859573be20e5bc850f61d6459bd436a8
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-09-13 22:46:56 -0500

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  motion,
+  MotionStyle,
+  useMotionValue,
+  useMotionValueEvent,
+  useTransform,
+} from "framer-motion";
+
+import { useState } from "react";
 
 function App() {
   return (
@@ -9,23 +17,63 @@ function App() {
 }
 
 function FoldedMap() {
+  const [isFolded, setIsFolded] = useState(true);
   const xDrag = useMotionValue(0);
   const xLeftSection = useTransform(xDrag, [0, 300], ["100%", "0%"]);
   const xRightSection = useTransform(xDrag, [0, 300], ["-100%", "0%"]);
+  const centerScale = useTransform(xDrag, [150, 300], [0, 1]);
+  const centerBrightness = useTransform(xDrag, [150, 300], [0.2, 1]);
+
+  useMotionValueEvent(xDrag, "change", (currentX) => {
+    if (currentX > 260) {
+      setIsFolded(false);
+    } else {
+      setIsFolded(true);
+    }
+  });
+
   return (
-    <div className="mx-auto grid aspect-video max-h-[80vh] p-8">
-      <div className="aspect-video grid grid-cols-3 [grid-area:1/1] h-full w-full">
-        <motion.div style={{ x: xLeftSection }} className="map-image" />
-        <div className="map-image" />
-        <motion.div style={{ x: xRightSection }} className="map-image" />
-      </div>
+    <section className="overflow-x-clip">
       <motion.div
-        drag="x"
-        dragConstraints={{ left: 0, right: 300 }}
-        style={{ x: xDrag }}
-        className="[grid-area:1/1] border border-blue-100 relative z-10"
-      />
-    </div>
+        animate={isFolded ? "folded" : "open"}
+        variants={{ open: { scale: 1 }, folded: { scale: 0.9 } }}
+        initial="folded"
+      >
+        <div className="mx-auto grid aspect-video max-h-[80vh] p-8">
+          <div className="aspect-video grid grid-cols-3 [grid-area:1/1] h-full w-full">
+            <motion.div
+              style={{ x: xLeftSection, skewY: "-1deg" }}
+              className="origin-bottom-right map-image border-r border-[rgba(255,255,255,0.1)] shadow-2xl"
+            />
+            <motion.div
+              style={
+                {
+                  scaleX: centerScale,
+                  "--brightness": centerBrightness,
+                } as MotionStyle
+              }
+              className="map-image brightness-[--brightness]"
+            />
+            <motion.div
+              style={{ x: xRightSection, skewY: "1deg" }}
+              className="origin-bottom-left map-image border-l border-[rgba(255,255,255,0.1)] shadow-2xl"
+            />
+          </div>
+          <motion.div
+            drag="x"
+            _dragX={xDrag}
+            dragConstraints={{ left: 0, right: 300 }}
+            dragTransition={{
+              modifyTarget: (target) => {
+                return target > 150 ? 300 : 0;
+              },
+              timeConstant: 45,
+            }}
+            className="[grid-area:1/1]  relative z-10"
+          />
+        </div>
+      </motion.div>
+    </section>
   );
 }
 
